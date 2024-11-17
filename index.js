9698ea210a79acc263f662e9d270c6b4f3545514
Julian So <ch.julian.so@gmail.com>
2024-11-17 13:24:55 -0500

    write basic node routing

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 import { createServer } from 'node:http';
-import * as fs from 'node:fs';
+import * as fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
 
-const server = createServer((req, res) => {
-  fs.readFile('index.html', function (err, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+const PORT = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const server = createServer(async (req, res) => {
+  if (req.method === 'GET') {
+    let filePath;
+    if (req.url === '/' || req.url === '/index.html') {
+      filePath = path.join(__dirname, 'index.html');
+    } else if (req.url === '/about.html') {
+      filePath = path.join(__dirname, 'about.html');
+    } else if (req.url === '/contact-me.html') {
+      filePath = path.join(__dirname, 'contact-me.html');
+    } else {
+      filePath = path.join(__dirname, '404.html');
+    }
+
+    const data = await fs.readFile(filePath);
+    res.setHeader('Content-Type', 'text/html');
     res.write(data);
-    return res.end();
-  });
+    res.end();
+  }
 });
 
-server.listen(8080);
+server.listen(PORT);
