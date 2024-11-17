7b44a2768aa0d1a81fd86fc2f3abad7f820c01e0
Julian So <ch.julian.so@gmail.com>
2024-11-17 17:01:38 -0500

    test other ways to write routes

--- index.js
+++ index.js
@@ -8,23 +8,41 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = createServer(async (req, res) => {
-  if (req.method === 'GET') {
-    let filePath;
-    if (req.url === '/' || req.url === '/index.html') {
-      filePath = path.join(__dirname, 'index.html');
-    } else if (req.url === '/about.html') {
-      filePath = path.join(__dirname, 'about.html');
-    } else if (req.url === '/contact-me.html') {
-      filePath = path.join(__dirname, 'contact-me.html');
-    } else {
-      filePath = path.join(__dirname, '404.html');
-    }
+  try {
+    if (req.method === 'GET') {
+      let filePath;
+      if (req.url === '/' || req.url === '/index.html') {
+        filePath = path.join(__dirname, 'index.html');
+      } else if (req.url === '/about.html') {
+        filePath = path.join(__dirname, 'about.html');
+      } else if (req.url === '/contact-me.html') {
+        filePath = path.join(__dirname, 'contact-me.html');
+      } else {
+        filePath = path.join(__dirname, '404.html');
+      }
 
-    const data = await fs.readFile(filePath);
-    res.setHeader('Content-Type', 'text/html');
-    res.write(data);
-    res.end();
+      const data = await fs.readFile(filePath);
+      console.log(filePath);
+      res.setHeader('Content-Type', 'text/html');
+      res.write(data);
+      res.end();
+    }
+  } catch (err) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.end('Server Error');
   }
+
+  // const data = await fs.readFile(filePath, (err, data) => {
+  // await fs.readFile(filePath, (err, data) => {
+  //   if (err) {
+  //     res.writeHead(500, { 'Content-Type': 'text/plain' });
+  //   }
+
+  //   // res.setHeader('Content-Type', 'text/html');
+  //   res.writeHead(200, { 'Content-Type': 'text/html' });
+  //   res.write(data);
+  //   res.end();
+  // });
 });
 
 server.listen(PORT);
